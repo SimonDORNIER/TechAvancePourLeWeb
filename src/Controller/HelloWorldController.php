4e015920c8eb62d3e49e11a19251add5819e4cc4
Simon Dornier <simondornier98@gmail.com>
2021-01-11 08:11:00 +0100

    maj

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -4,7 +4,52 @@
 namespace App\Controller;
 
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class HelloWorldController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
 {
+    /**
+     * Controller "hello world", affiche la chaîne "Hello World" dans le navigateur
+     * @param string $prenom le prénom à afficher
+     * @return Response
+     */
+    function hello($prenom):Response{
+        return $this->render('hello.html.twig', [
+            'prenom'=>$prenom
+        ]);
+    }
 
+    /**
+     * Affiche un formulaire
+     * @return Response
+     */
+    function form():Response{
+
+        return new Response("<html>
+    <body>
+        <form method='post'>
+            Nom : <input name='name'/>
+            <input type='submit'/>
+        </form>
+    </body>
+</html>");
+
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    function formReceive(Request $request):Response{
+        $formData = $request->request->get('name');
+        return new Response("Merci $formData");
+    }
+
+    function liste():Response{
+        $liste = ['Bernard', 'Jean', 'Daniel', 'Patrick'];
+        return $this->render('liste.html.twig', [
+            'liste'=>$liste
+        ]);
+    }
 }
